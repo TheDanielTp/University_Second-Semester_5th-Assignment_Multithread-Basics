2e311db9bea0bd6a281b2c6038d1acbe3aa6b148
TheDanielTp <prof.danial4@gmail.com>
2024-05-09 18:26:47 +0330

    Initialized constructor & run functions

--- src/main/java/sbu/cs/TaskScheduler.java
+++ src/main/java/sbu/cs/TaskScheduler.java
@@ -1,51 +1,43 @@
 package sbu.cs;
 
 import java.util.ArrayList;
-import java.util.List;
 
 public class TaskScheduler
 {
     public static class Task implements Runnable
     {
-        /*
-            ------------------------- You don't need to modify this part of the code -------------------------
-         */
         String taskName;
-        int processingTime;
+        int    processingTime;
 
-        public Task(String taskName, int processingTime) {
-            this.taskName = taskName;
+        public Task (String taskName, int processingTime)
+        {
+            this.taskName       = taskName;
             this.processingTime = processingTime;
         }
-        /*
-            ------------------------- You don't need to modify this part of the code -------------------------
-         */
 
         @Override
-        public void run() {
-            /*
-            TODO
-                Simulate utilizing CPU by sleeping the thread for the specified processingTime
-             */
+        public void run ()
+        {
+            try
+            {
+                System.out.println ("Executing task: " + taskName);
+                Thread.sleep (processingTime);
+                System.out.println ("Completed task: " + taskName);
+            }
+            catch (InterruptedException e)
+            {
+                e.printStackTrace ();
+            }
         }
     }
 
-    public static ArrayList<String> doTasks(ArrayList<Task> tasks)
+    public static ArrayList <String> doTasks (ArrayList <Task> tasks)
     {
-        ArrayList<String> finishedTasks = new ArrayList<>();
-
-        /*
-        TODO
-            Create a thread for each given task, And then start them based on which task has the highest priority
-            (highest priority belongs to the tasks that take more time to be completed).
-            You have to wait for each task to get done and then start the next task.
-            Don't forget to add each task's name to the finishedTasks after it's completely finished.
-         */
-
-        return finishedTasks;
+        return null;
     }
 
-    public static void main(String[] args) {
-        // Test your code here
+    public static void main (String[] args)
+    {
+
     }
 }
