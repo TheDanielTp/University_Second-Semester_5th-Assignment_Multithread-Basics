774d71ae17ea6decc91e2662b530e7de7405398d
TheDanielTp <prof.danial4@gmail.com>
2024-05-09 18:30:16 +0330

    Initialized ParallelizeMatMul function

--- src/main/java/sbu/cs/MatrixMultiplication.java
+++ src/main/java/sbu/cs/MatrixMultiplication.java
@@ -1,5 +1,6 @@
 package sbu.cs;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class MatrixMultiplication {
@@ -54,15 +55,62 @@ public class MatrixMultiplication {
     Matrix B is of the form q x r
     both p and r are even numbers
     */
-    public static List<List<Integer>> ParallelizeMatMul(List<List<Integer>> matrix_A, List<List<Integer>> matrix_B)
+    public static List <List <Integer>> ParallelizeMatMul (List <List <Integer>> matrix_A, List <List <Integer>> matrix_B)
     {
-        /*
-        TODO
-            Parallelize the matrix multiplication by dividing tasks between 4 threads.
-            Each thread should calculate one block of the final matrix product. Each block should be a quarter of the final matrix.
-            Combine the 4 resulting blocks to create the final matrix product and return it.
-         */
-        return null;
+        int p = matrix_A.size ();
+        int r = matrix_B.getFirst ().size ();
+
+        if (p % 2 != 0 || r % 2 != 0)
+        {
+            throw new IllegalArgumentException ("Dimensions of matrices A and B must be even.");
+        }
+
+        //set the matrix default value to zero
+        List <List <Integer>> tempMatrixProduct = new ArrayList <> (p);
+        for (int i = 0; i < p; i++)
+        {
+            List <Integer> row = new ArrayList <> (r);
+            for (int j = 0; j < r; j++)
+            {
+                row.add (0);
+            }
+            tempMatrixProduct.add (row);
+        }
+
+        int quarterSize = p / 2;
+
+        //create and start threads for each quarter
+        Thread[] threads = new Thread[4];
+        for (int i = 0; i < 2; i++)
+        {
+            for (int j = 0; j < 2; j++)
+            {
+                int startRow = i * quarterSize;
+                int endRow   = startRow + quarterSize;
+                int startCol = j * quarterSize;
+                int endCol   = startCol + quarterSize;
+
+                BlockMultiplier blockMultiplier = new BlockMultiplier (matrix_A, matrix_B, tempMatrixProduct, startRow, endRow, startCol, endCol);
+
+                threads[i * 2 + j] = new Thread (blockMultiplier);
+                threads[i * 2 + j].start ();
+            }
+        }
+
+        //wait for all threads to finish their task
+        for (Thread thread : threads)
+        {
+            try
+            {
+                thread.join ();
+            }
+            catch (InterruptedException e)
+            {
+                System.out.println (e.getMessage ());
+            }
+        }
+
+        return tempMatrixProduct;
     }
 
     public static void main(String[] args) {
